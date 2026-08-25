8997730be57e768d4d4362b04ae2578beebdb483
GokulAnand14 <galliumcreations@outlook.com>
2026-08-25 17:53:40 +0530

    fix(windows): suppress background terminal window using windows_subsystem attribute

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,6 @@
+// Prevents additional console window on Windows when launching the application
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main() {
     excalideck_lib::run();
 }
